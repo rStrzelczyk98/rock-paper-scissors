abbaa4c14a93626cb2d0a45b25828a03f5fca4cd
Robert Strzelczyk <robertstrzelczyk98@gmail.com>
2023-04-07 10:20:59 +0200

    update

--- script.js
+++ script.js
@@ -70,7 +70,6 @@ function update(num, winner) {
   overflow();
   if (index && landscape) container.children[index].classList.add(`shadow-2`);
   if (index && !landscape) container.children[index].classList.add(`shadow-1`);
-  console.log(index);
   if (index === 0) window.navigator.vibrate([200, 100, 200, 100, 200]);
   setTimeout(() => updateScore(winner), 500);
 }
